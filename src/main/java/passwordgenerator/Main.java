1bc98f57a7eda29f7ceea0ad00c82d2de2577b8d
Supra_Tony <s.franck@absoftwork.com>
2026-06-25 23:40:44 +0000

    Ajout renommage de getInputUser en getNumberUserInput pour plus de clarté, puis ajout et teste de la méthode permettant de demander à l'utilisateur de faire un choix

--- src/main/java/passwordgenerator/Main.java
+++ src/main/java/passwordgenerator/Main.java
@@ -5,16 +5,52 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("BIENVENUE SUR PASSWORD GENERATOR VOTRE GÉNÉRATEUR DE MOT DE PASSE");
         Scanner scanner = new Scanner(System.in);
-        int passwordCount = getUserInput(scanner, "Combien de mots de passe veux-tu générer : ");
-        int passwordLength = getUserInput(scanner,"Veuillez entrez la longueur du mot de passe : ");
+        int passwordCount = getNumberUserInput(scanner, "Combien de mots de passe veux-tu générer : ");
+        int passwordLength = getNumberUserInput(scanner,"Veuillez entrez la longueur du mot de passe : ");
+
+        boolean upperCaseCharater = getBooleanUserInput(scanner,"Inclure les majuscules ?");
+        boolean lowerCaseCharacter = getBooleanUserInput(scanner,"Inclure les minuscules ?");
+        boolean integerCharacter = getBooleanUserInput(scanner,"Inclure les chiffres ?");
+        boolean symbolCharater = getBooleanUserInput(scanner, "Inclure les symboles ?");
+
+
         System.out.println("Longueur choisie : " + passwordLength);
         System.out.println("Nombre demandé : " + passwordCount);
+        System.out.println("Majuscule: " + upperCaseCharater);
+        System.out.println("Minuscule: " + lowerCaseCharacter);
+        System.out.println("Nombre : " + integerCharacter);
+        System.out.println("Symbol : " + symbolCharater);
         scanner.close();
     }
 
-
-    private static int getUserInput(Scanner scanner, String message){
-        System.out.print(message);
-        return Integer.parseInt(scanner.nextLine());
+    // Elle évite de répéter plusieurs fois le même code de saisie.
+    private static int getNumberUserInput(Scanner scanner, String message){
+        while(true) { //Permet de vérifier que l'utilisateur saisisse des valeurs qui ferait planter l'application
+            System.out.print(message);
+            String valeur = scanner.nextLine();
+            try {
+                int nombre = Integer.parseInt(valeur);
+                if(nombre > 0) {
+                    return nombre;
+                }
+                System.out.println( "Le nombre doit être supérieur à 0.");
+            } catch(NumberFormatException e) {
+                System.out.println("Veuillez entrer un nombre valide.");
+            }
+        }
+    }
+    // Cette méthode transforme la réponse utilisateur en une valeur true ou false utilisable dans le programme.
+    private static boolean getBooleanUserInput(Scanner scanner,String message){
+        while(true){
+            System.out.print(message + " (O/N) : ");
+            String reponse = scanner.nextLine().trim().toLowerCase();
+            if(reponse.equals("O")){
+                return true;
+            }
+            if(reponse.equals("N")){
+                return false;
+            }
+            System.out.println("Votre réponse est invalide");
+        }
     }
 }
