d8c6a66e18c024528f7b7bf7a1e8b62aae11f0ed
Supra_Tony <s.franck@absoftwork.com>
2026-06-25 23:12:35 +0000

    Ajout et teste de la saisie utilisateur permattant de demander et de récupérer les valeurs saisie par l'utilisateur

--- src/main/java/passwordgenerator/Main.java
+++ src/main/java/passwordgenerator/Main.java
@@ -1,8 +1,20 @@
 package passwordgenerator;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Password Generator");
+        System.out.println("BIENVENUE SUR PASSWORD GENERATOR VOTRE GÉNÉRATEUR DE MOT DE PASSE");
+        Scanner scanner = new Scanner(System.in);
+        int passwordCount = getUserInput(scanner, "Combien de mots de passe veux-tu générer : ");
+        int passwordLength = getUserInput(scanner,"Veuillez entrez la longueur du mot de passe : ");
+        System.out.println("Longueur choisie : " + passwordLength);
+        System.out.println("Nombre demandé : " + passwordCount);
+        scanner.close();
+    }
+
+
+    private static int getUserInput(Scanner scanner, String message){
+        System.out.print(message);
+        return Integer.parseInt(scanner.nextLine());
     }
 }
